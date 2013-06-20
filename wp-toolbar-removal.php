57cc64781fe5ab62493d1099376e45895ed0a57d
sLaNGjI <sLaNGjI@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-20 13:27:42 +0000

    Version 2013.0620-BUGFIX.0526-DEVELOPMENTAL
    git-svn-id: https://plugins.svn.wordpress.org/wp-toolbar-removal/trunk@728874 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-toolbar-removal.php
+++ wp-toolbar-removal.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: √ WP ToolBar Removal
+Plugin Name: √ WP ToolBar Removal - DEV
 Plugin URI: http://slangji.wordpress.com/wp-toolbar-removal/
 Description: Completely <code>Disable</code> new WordPress 3.5+ / 3.4+ / 3.3+ (only) Admin <code>ToolBar</code> Frontend, Backend, Node, Pointer, Shadow, User Profile, without loosing Logout and Network functionality! Completely <code>Remove Code</code> for Minimal Memory Load, and Dasboard Speedup, with new approach. Thanks to olyma! | <a href="http://slangji.wordpress.com/donate/" title="Free Donation">Donate</a> | <a href="http://slangji.wordpress.com/contact/" title="Send Me Bug and Suggestions">Contact</a> | <a href="http://wordpress.org/plugins/wp-overview-lite/" title="Show Dashboard Overview and Footer Memory Load Usage">WP Overview?</a> | <a href="http://wordpress.org/plugins/wp-missed-schedule/" title="Fix Missed Scheduled Future Posts Cron Job">WP Missed Schedule?</a> | <a href="http://wordpress.org/plugins/wp-admin-bar-removal/" title="Remove Admin Bar Frontend Backend User Profile and Code">Admin Bar Removal?</a> | <a href="http://wordpress.org/plugins/wp-admin-bar-node-removal/" title="Remove Admin Bar Frontend and Backend Node">Admin Bar Node Removal?</a> | <a href="http://wordpress.org/plugins/wp-toolbar-removal/" title="Remove ToolBar Frontend Backend User Profile and Code">ToolBar Removal?</a> | <a href="http://wordpress.org/plugins/wp-toolbar-node-removal/" title="Remove ToolBar Frontend and Backend Node">ToolBar Node Removal?</a> | <a href="http://wordpress.org/plugins/wp-login-deindexing/" title="Total DeIndexing WordPress LogIn from all Search Engines">LogIn DeIndexing?</a> | <a href="http://wordpress.org/plugins/wp-total-deindexing/" title="Total DeIndexing WordPress from all Search Engines">WP DeIndexing?</a> | <a href="http://wordpress.org/plugins/wp-ie-enhancer-and-modernizer/" title="Enhancer and Modernizer IE Surfing Expirience">Enhancer IE Surfing?</a>
 Version: 2013.0620-BUGFIX.0526-DEVELOPMENTAL
@@ -54,7 +54,7 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
  * one-click-logout-barless.php by olyma <rackofpower.com>
  * Copyright (C) 2011 [olyma](http://rackofpower.com/) (email: <olyma[at]rackofpower[dot]com>)
  *
- * wp-toolbar-removal-node-addon by sLa <slangji[at]gmail[dot]com>
+ * wp-toolbar-removal-node-addon.php by sLa <slangji[at]gmail[dot]com>
  * Copyright (C) 2012-2013 [sLaNGjI's](http://profiles.wordpress.org/slangji) (email: <slangji[at]gmail[dot]com>)
  *
  * according to the terms of the GNU General Public License version 2 (or later)
@@ -134,22 +134,29 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 	 * @keytag 74be16979710d4c4e7c6647856088456
 	 */
 
-		if ( !function_exists( 'add_action' ) )
-			{
-				header( 'HTTP/0.9 403 Forbidden' );
-				header( 'HTTP/1.0 403 Forbidden' );
-				header( 'HTTP/1.1 403 Forbidden' );
-				header( 'Status: 403 Forbidden' );
-				header( 'Connection: Close' );
-					exit();
-			}
+	if ( !function_exists( 'add_action' ) )
+		{
+			header( 'HTTP/0.9 403 Forbidden' );
+			header( 'HTTP/1.0 403 Forbidden' );
+			header( 'HTTP/1.1 403 Forbidden' );
+			header( 'Status: 403 Forbidden' );
+			header( 'Connection: Close' );
+				exit();
+		}
 
-			global $wp_version;
+	global $wp_version;
 
-			if ( $wp_version < 3.3 )
-				{
-					wp_die( __( 'This plugin requires WP 3.3+ or greater: for 3.1+ use <a href="http://wordpress.org/plugins/wp-admin-bar-removal/">Admin Bar Removal</a> or <a href="http://wordpress.org/plugins/toolbar-removal-completely-disable/">Disable Admin Bar and ToolBar</a>' ) );
-				}
+	if ( $wp_version < 3.1 )
+		{
+			wp_die( __( 'This plugin requires WordPress 3.1+ or greater.' ) );
+		}
+
+	//global $wp_version;
+
+	//if ( $wp_version < 3.3 )
+	//	{
+	//		wp_die( __( 'This plugin requires WP 3.3+ or greater: for 3.1+ use <a href="http://wordpress.org/plugins/wp-admin-bar-removal/">Admin Bar Removal</a> or <a href="http://wordpress.org/plugins/toolbar-removal-completely-disable/">Disable Admin Bar and ToolBar</a>' ) );
+	//	}
 
 	function wptbr_ras()
 		{
@@ -163,8 +170,8 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 		}
 	add_action( 'admin_print_styles', 'wptbr_rsb', 21 );
 
-		function wptrcd_ablhdr()
-			{
+	function wptbr_ablhdr()
+		{
 
 ?>
 <style type="text/css">table#wptbr td#wptbr_ttl a:link,table#wptbr td#wptbr_ttl a:visited{text-decoration:none}table#wptbr td#wptbr_lgt,table#wptbr td#wptbr_lgt a{text-decoration:none}</style>
@@ -173,7 +180,7 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 <td align="left" valign="center" id="wptbr_ttl">
 <?php
 
-		echo '<a href="' . home_url() . '">' . __( get_bloginfo() ) . '</a>';
+	echo '<a href="' . home_url() . '">' . __( get_bloginfo() ) . '</a>';
 
 ?>
 </td>
@@ -181,40 +188,40 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 <div style="padding-top:2px">
 <?php
 
-		if ( function_exists( 'sLa_pml' ) )
-			{
-				echo (sLa_pml() . 'M - ');
-			}
+	if ( function_exists( 'sLa_pml' ) )
+		{
+			echo ( sLa_pml() . 'M - ' );
+		}
 
-		if ( function_exists( 'sLa_pcl' ) )
-			{
-				echo (sLa_pcl() . ' - ');
-			}
+	if ( function_exists( 'sLa_pcl' ) )
+		{
+			echo ( sLa_pcl() . ' - ' );
+		}
 
-		echo date( 'l j F Y @ H:i' );
+	echo date( 'l j F Y @ H:i' );
 
-		wp_get_current_user();
+	wp_get_current_user();
 
-		$current_user = wp_get_current_user();
+	$current_user = wp_get_current_user();
 
-		if ( !( $current_user instanceof WP_User ) )
-			return;
+	if ( !( $current_user instanceof WP_User ) )
+		return;
 
-		echo ' | ' . $current_user->display_name . '';
+	echo ' | ' . $current_user->display_name . '';
 
-		if ( is_multisite() && is_super_admin() )
-			{
-				if ( !is_network_admin() )
-					{
-						echo ' | <a href="' . network_admin_url() . '">' . __( 'Network Admin' ) . '</a>';
-					}
-				else
-					{
-						echo ' | <a href="' . get_Dashboard_url( get_current_user_id() ) . '">' . __( 'Site Admin' ) . '</a>';
-					}
-			}
+	if ( is_multisite() && is_super_admin() )
+		{
+			if ( !is_network_admin() )
+				{
+					echo ' | <a href="' . network_admin_url() . '">' . __( 'Network Admin' ) . '</a>';
+				}
+			else
+				{
+					echo ' | <a href="' . get_Dashboard_url( get_current_user_id() ) . '">' . __( 'Site Admin' ) . '</a>';
+				}
+		}
 
-		echo ' | <a href="' . wp_logout_url( home_url() ) . '">' . __( 'Log Out' ) . '</a>';
+	echo ' | <a href="' . wp_logout_url( home_url() ) . '">' . __( 'Log Out' ) . '</a>';
 
 ?>
 </div>
@@ -225,9 +232,9 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 </table>
 <?php
 
-			}
+		}
 
-	add_action( 'in_admin_header', 'wptbr_hdr' );
+	add_action( 'in_admin_header', 'wptbr_ablhdr' );
 
 	function wptbr_rbcb()
 		{
@@ -240,13 +247,17 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 
 	show_admin_bar( false );
 
-	function wptbr_init()
+	function wp_toolbar_init()
 		{
 			add_filter( 'show_admin_bar', '__return_false' );
 			add_filter( 'wp_admin_bar_class', '__return_false' );
+			add_filter( 'wp_admin_bar_css', '__return_false' );
+			add_filter( 'wp_admin_bar_js', '__return_false' );
+			add_filter( 'wp_admin_bar_render', '__return_false' );
+			add_filter( 'wp_admin_bar_ajax_render', '__return_false' );
 			add_filter( 'show_wp_pointer_admin_bar', '__return_false' );
 		}
-	add_filter( 'init', 'wptbr_init', 9 );
+	add_filter( 'init', 'wp_toolbar_init', 9 );
 
 	function wptbr_ruppoabpc()
 		{
@@ -255,10 +266,10 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 	add_action( 'admin_print_styles-profile.php', 'wptbr_ruppoabpc' );
 
 	$wp_scripts = new WP_Scripts();
-	wp_deregister_script('admin-bar');
+	wp_deregister_script( 'admin-bar' );
 
 	$wp_styles = new WP_Styles();
-	wp_deregister_style('admin-bar');
+	wp_deregister_style( 'admin-bar' );
 
 	remove_action('init', 'wp_admin_bar_init');
 	remove_filter('init', 'wp_admin_bar_init');
@@ -280,15 +291,6 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 	remove_filter('admin_head', 'wp_admin_bar_class');
 	remove_action('admin_footer', 'wp_admin_bar_class');
 	remove_filter('admin_footer', 'wp_admin_bar_class');
-
-	remove_action('wp_head', 'wp_admin_bar_render', 1000);
-	remove_filter('wp_head', 'wp_admin_bar_render', 1000);
-	remove_action('wp_footer', 'wp_admin_bar_render', 1000);
-	remove_filter('wp_footer', 'wp_admin_bar_render', 1000);
-	remove_action('admin_head', 'wp_admin_bar_render', 1000);
-	remove_filter('admin_head', 'wp_admin_bar_render', 1000);
-	remove_action('admin_footer', 'wp_admin_bar_render', 1000);
-	remove_filter('admin_footer', 'wp_admin_bar_render', 1000);
 
 	remove_action('wp_head', 'wp_admin_bar_css');
 	remove_filter('wp_head', 'wp_admin_bar_css');
@@ -316,36 +318,44 @@ Indentation URI: http://www.gnu.org/prep/standards/standards.html
 	remove_action('admin_footer', 'wp_admin_bar_dev_js');
 	remove_filter('admin_footer', 'wp_admin_bar_dev_js');
 
-	remove_action('wp_ajax_adminbar_render', 'wp_admin_bar_ajax_render');
-	remove_filter('wp_ajax_adminbar_render', 'wp_admin_bar_ajax_render');
-
 	remove_action('personal_options', '_admin_bar_pref');
 	remove_filter('personal_options', '_admin_bar_pref');
-
 	remove_action('personal_options', '_get_admin_bar_pref');
 	remove_filter('personal_options', '_get_admin_bar_pref');
 
 	remove_action('locale', 'wp_admin_bar_lang');
 	remove_filter('locale', 'wp_admin_bar_lang');
 
+	remove_action('wp_head', 'wp_admin_bar_render', 1000);
+	remove_filter('wp_head', 'wp_admin_bar_render', 1000);
+	remove_action('wp_footer', 'wp_admin_bar_render', 1000);
+	remove_filter('wp_footer', 'wp_admin_bar_render', 1000);
+	remove_action('admin_head', 'wp_admin_bar_render', 1000);
+	remove_filter('admin_head', 'wp_admin_bar_render', 1000);
+	remove_action('admin_footer', 'wp_admin_bar_render', 1000);
+	remove_filter('admin_footer', 'wp_admin_bar_render', 1000);
+
+	remove_action('wp_ajax_adminbar_render', 'wp_admin_bar_ajax_render');
+	remove_filter('wp_ajax_adminbar_render', 'wp_admin_bar_ajax_render');
+
 	remove_action('admin_footer', 'wp_admin_bar_render');
 	remove_filter('admin_footer', 'wp_admin_bar_render');
 
-		function wptbr_nfo()
-			{
-				echo "\n<!--Plugin Disable Admin Bar and ToolBar 2013.0620-BUGFIX.0526-DEVELOPMENTAL Active - Tag: ".md5(md5("".""))."-->\n";
-				echo "\n<!--This Site is Optimized to Speedup Dashboard and Minimize Memory Load Consumption";
+	function wptbr_nfo()
+		{
+			echo "\n<!--Plugin ToolBar Removal 2013.0620-BUGFIX.0526-DEVELOPMENTAL Active - Tag: ".md5(md5("".""))."-->\n";
+			echo "\n<!--This Site is Optimized to Speedup Dashboard and Minimize Memory Load Consumption";
 
-				global $wp_version;
+			global $wp_version;
 
-				if ( $wp_version >= 3.3 )
-							{
-								echo " with ToolBar Disabled";
-							}
+			if ( $wp_version >= 3.3 )
+				{
+					echo " with ToolBar Disabled";
+				}
 
-				echo "-->\n\n";
-			}
-			add_action('wp_head', 'wptbr_nfo');
-			add_action('wp_footer', 'wptbr_nfo');
+			echo "-->\n\n";
+		}
+		add_action( 'wp_head', 'wptbr_nfo' );
+		add_action( 'wp_footer', 'wptbr_nfo' );
 
 ?>
