ade3270f9ca61d7943e51cead41ab0ebde1375d7
slangjis <slangjis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-18 14:52:34 +0000

    Version 2014.0507.0391
    git-svn-id: https://plugins.svn.wordpress.org/wp-toolbar-removal/trunk@934508 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-toolbar-removal.js
@@ -0,0 +1,10 @@
+
+/*
+Target Version: 2014.1210.0410
+*/
+
+	/**
+	 * Contact me to know how to get this file and test new features
+	 * that will be developed offline @ slangji.wordpress.com/contact/
+	 */
+
