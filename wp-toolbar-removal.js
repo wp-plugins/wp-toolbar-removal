f7871ff4b49bd83232e9e077de18548ac1714ecf
slangjis <slangjis@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-08 18:11:11 +0000

    Version 2014.0708.0383
    git-svn-id: https://plugins.svn.wordpress.org/wp-toolbar-removal/tags/2014.0708.0383@945120 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-toolbar-removal.js
@@ -0,0 +1,24 @@
+
+/*
+ * @package WP Toolbar Removal
+ * @subpackage External JS Enqueue Script
+ * @branche 2014
+ * @build   2014-07-08
+ * @since   3.8.3
+ * @tested  3.8.3
+ * @version 2014.0708.0383
+ * @target  2014.1210.0410
+ * @status STABLE (trunk) release
+ * @development Code in Becoming!
+ * @author slangjis
+ * @license GPLv2 or later
+ * @indentation GNU style coding standard
+*/
+
+	/**
+	 * Please contact me @ slangji.wordpress.com/contact/
+	 * to know how to get this file and test new features
+	 * that will be developed offline on github.com/slangji
+	 */
+
+
